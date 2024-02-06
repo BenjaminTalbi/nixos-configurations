1ca733cd4b95ec8c408db1f427abcbd7e6a6473f
Benjamin Talbi <b.talbi@live.de>
2024-02-06 18:48:26 +0100

    Change wezterm highlight colors

--- home/terminal/wezterm.nix
+++ home/terminal/wezterm.nix
@@ -5,26 +5,29 @@
     enable = true;
     package = inputs.wezterm.packages.${pkgs.system}.default;
     extraConfig = ''
-        local config = {}
-        if wezterm.config_builder then
-          config = wezterm.config_builder()
-        end
+      local config = {}
+      if wezterm.config_builder then
+        config = wezterm.config_builder()
+      end
 
-        local rose_pine = wezterm.color.get_builtin_schemes()['rose-pine']
+      local rose_pine = wezterm.color.get_builtin_schemes()['rose-pine']
 
-        rose_pine.copy_mode_active_highlight_bg = { Color = '#ebbcba' }
-        rose_pine.copy_mode_active_highlight_fg = { Color = '#21202e' }
-        rose_pine.copy_mode_inactive_highlight_bg = { Color = '#524f67' }
-        rose_pine.copy_mode_inactive_highlight_fg = { Color = '#21202e' }
+      rose_pine.selection_fg = '#191724'
+      rose_pine.selection_bg = '#6e6a86'
 
-        config.color_scheme = 'rose-pine'
+        
+      config.color_schemes = {
+        ['rose-pine-custom'] = rose_pine
+      }
 
-        config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'Font Awesome 6 Free' }
-        config.font_size = 16.0
-        config.window_background_opacity = 0.97
-        config.adjust_window_size_when_changing_font_size = false
+      config.color_scheme = 'rose-pine-custom'
+
+      config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font Mono', 'Font Awesome 6 Free' }
+      config.font_size = 16.0
+      config.window_background_opacity = 0.97
+      config.adjust_window_size_when_changing_font_size = false
       
-        return config
+      return config
     '';
   };
 }
