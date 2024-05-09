70b3bfb2d56b53faf3caab37113d3e39fb8fc027
System administrator <root@nixos.>
2024-05-09 23:27:57 +0200

    Add basic config for WSL

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    
+    # Needed for Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Needed for some vim plugins (ie. vim-extra-plugins)
+    nur.url = "github:nix-community/nur";
+
+    # Needed for nixvim managed through home-manager
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    # WSL dependency
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: 
+  let
+    userSettings = {
+      username = "benjamin";
+      name = "benjamin";
+      email = "b.talbi@live.de";
+      term = "foot";
+      editor = "vim";
+    };
+
+    systemSettings = {
+      timezone = "Europe/Berlin";
+      locale = "en_US.UTF-8";
+    };
+
+    extraSpecialArgs = {
+      inherit userSettings;
+      inherit systemSettings;
+      inherit inputs;
+    };
+
+    specialArgs = {
+      inherit userSettings;
+      inherit systemSettings;
+      inherit inputs;
+    };
+
+    pkgs = nixpkgs;
+  in
+  {
+    nixosConfigurations = {
+      frija = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = pkgs.lib.recursiveUpdate specialArgs { systemSettings.hostname = "frija"; };
+
+        modules = [
+          ./hosts/frija/configuration.nix
+	  nixos-wsl.nixosModules.wsl
+        ];
+      };
+    };
+  };
+
+  nixConfig = {
+    substituters = [
+      "https://hyprland.cachix.org" # Cache for Hyprland specifically
+      "https://cache.nixos.org" # Cache for everything else
+    ];
+
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+
+    trusted-users = [ "benjamin" ];
+  };
+  
+}
---
+++ hosts/frija/configuration.nix
@@ -0,0 +1,61 @@
+{ lib, pkgs, systemSettings, userSettings, ... }:
+
+{
+  imports = [];
+  
+  wsl = {
+    enable = true;
+    wslConf.automount.root = "/mnt";
+    defaultUser = userSettings.username;
+    startMenuLaunchers = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  # Ensure nix flakes are enabled
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  
+  networking.hostName = systemSettings.hostname;
+
+  # Timezone & Locale  
+  time.timeZone = systemSettings.timezone;
+
+  i18n.defaultLocale = systemSettings.locale;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = systemSettings.locale;
+    LC_IDENTIFICATION = systemSettings.locale;
+    LC_MEASUREMENT = systemSettings.locale;
+    LC_MONETARY = systemSettings.locale;
+    LC_NAME = systemSettings.locale;
+    LC_NUMERIC = systemSettings.locale;
+    LC_PAPER = systemSettings.locale;
+    LC_TELEPHONE = systemSettings.locale;
+    LC_TIME = systemSettings.locale;
+  };
+
+  # User account
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.name;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    uid = 1000;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  environment.enableAllTerminfo = true;
+
+  environment.systemPackages = with pkgs; [
+    (import ./win32yank.nix {inherit pkgs;})
+    vim
+    wget
+    git
+    home-manager
+  ];
+  
+  system.stateVersion = "23.11";
+}
