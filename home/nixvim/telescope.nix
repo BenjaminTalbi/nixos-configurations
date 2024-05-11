fc8ede7731169eb97ac3e7a4383ae69063798652
Benjamin Talbi <b.talbi@live.de>
2024-05-11 19:15:52 +0200

    [nixvim] Add descriptions for keybinds for telescope

--- home/nixvim/telescope.nix
+++ home/nixvim/telescope.nix
@@ -1,25 +1,47 @@
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
-    settings.defaults = {
-      mappings = {
-        i =
-          let
-            actions = "require('telescope.actions')";
-          in
-          {
-            "<C-j>".__raw = "${actions}.move_selection_next";
-            "<C-k>".__raw = "${actions}.move_selection_previous";
-            "<C-q>".__raw = "${actions}.send_selected_to_qflist + ${actions}.open_qflist";
+    settings.defaults.mappings = {
+      i =
+        let
+          actions = "require('telescope.actions')";
+        in
+        {
+          "<C-j>" = {
+            __raw = "${actions}.move_selection_next";
+            desc = "Move to next selection";
           };
-      };
+          "<C-k>" = {
+            __raw = "${actions}.move_selection_previous";
+            desc = "Move to previous selection";
+          };
+          "<C-q>" = {
+            __raw = "${actions}.send_selected_to_qflist + ${actions}.open_qflist";
+            desc = "Send to quick fix list";
+          };
+        };
     };
     keymaps = {
-      "<leader>ff" = "find_files";
-      "<leader>fr" = "oldfiles";
-      "<leader>fw" = "grep_string";
-      "<leader>fs" = "live_grep";
-      "<C-p>" = "git_files";
+      "<leader>ff" = {
+        action = "find_files";
+        options.desc = "Find files";
+      };
+      "<leader>fr" = {
+        action = "oldfiles";
+        options.desc = "Recently used files";
+      };
+      "<leader>fw" = {
+        action = "grep_string";
+        options.desc = "Find word";
+      };
+      "<leader>fs" = {
+        action = "live_grep";
+        options.desc = "Find search term";
+      };
+      "<leader>fp" = {
+        action = "git_files";
+        options.desc = "Fuzzy search for files tracked by Git";
+      };
     };
     extensions.fzf-native = {
       enable = true;
