602c93650e176d200996c1fcf6af333b106bb5fb
Benjamin Talbi <b.talbi@live.de>
2024-05-12 12:48:12 +0200

    Update WP

--- themes/test.sh
+++ themes/test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+unalias -a
+ls
 for i in $(ls -d */);
 do
   if curl --output /dev/null --silent --head --fail $(cat $i/backgroundurl.txt); then
     echo "$i background successfully downloads";
+    echo -n $(curl --output /dev/null --silent --head --fail $(cat $i/backgroundurl.txt)) | sha256sum;
   else
     echo -e "\033[0;31m$i background download fails\033[0m"
 fi
