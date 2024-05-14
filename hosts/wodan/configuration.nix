fdf77da37b619056d25aa4e55b5850202c49eb18
Benjamin Talbi <b.talbi@live.de>
2024-05-14 23:02:48 +0200

    Add umlaute

--- hosts/wodan/configuration.nix
+++ hosts/wodan/configuration.nix
@@ -11,8 +11,29 @@
     ../../system/security/automount.nix
   ];
 
-  environment.systemPackages = with pkgs; [ 
+  services.keyd = {
+    enable = true;
+    keyboards.default.settings = {
+      main = {
+        capslock = "overload(custom, esc)";
+        rightalt = "oneshot(custom)";
+      };
+      custom = {
+        h = "left";
+        k = "up";
+        j = "down";
+        l = "right";
+        a = "macro(compose a \")";
+        o = "macro(compose o \")";
+        u = "macro(compose u \")";
+        s = "macro(compose s s)";
+      };
+      "custom+shift" = { };
+    };
+  };
+  environment.systemPackages = with pkgs; [
     google-chrome
+    keyd
   ];
   nixpkgs.config.allowUnfree = true;
 
